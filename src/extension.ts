82bd7502b6cf2c65b89eac317e3ddc46bfed4d96
boan <gazagoal@gmail.com>
2021-12-19 19:02:40 -0500

    first

---
+++ src/extension.ts
@@ -0,0 +1,82 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// var regexHex = /^0x[0-9a-fA-F]+$/g;
+	// var regexHexc = /^[0-9a-fA-F]+[h]$/g;
+	// var regexDec = /^-?[0-9]+$/g;
+	// var regexBin = /^0b[01]+$/g;
+	const testPattern = /{(.+)}/;
+	let hover = vscode.languages.registerHoverProvider({ scheme: '*', language: '*' }, {
+		provideHover(document, position, token) {
+			const hoveredWord = document.getText(document.getWordRangeAtPosition(position, /{{.+}}/));
+			
+			console.log(hoveredWord);
+
+			const captured = hoveredWord.match(testPattern);
+			console.log(captured)
+			if (captured === null || captured.length < 2) {
+				return;
+			}
+			var markdownString = new vscode.MarkdownString();
+
+			markdownString.appendCodeblock(captured[1], 'javascript');
+
+
+			return {
+				contents: [markdownString]
+			};
+			// if (testPattern.test(hoveredWord.toString())) {
+
+			// 	console.log(hoveredWord);
+			// 	// var input: Number = Number(parseInt(hoveredWord.substring(2), 2).toString());
+			// 	markdownString.appendCodeblock(hoveredWord, 'javascript');
+
+			// 	return {
+			// 		contents: [markdownString]
+			// 	};
+			// }
+
+
+
+			// else if (regexHex.test(hoveredWord.toString()) || regexHexc.test(hoveredWord.toString())) {
+
+			// 	markdownString.appendCodeblock(`Dec:\n${parseInt(hoveredWord, 16)}\nBinary:\n${parseInt(hoveredWord, 16).toString(2)}`, 'javascript');
+
+			// 	return {
+			// 		contents: [markdownString]
+			// 	};
+			// }
+			// else if (regexDec.test(hoveredWord.toString())) {
+
+			// 	var input: Number = Number(hoveredWord.toString());
+			// 	markdownString.appendCodeblock(`Hex:\n0x${input.toString(16).toUpperCase()}\nBinary:\n${input.toString(2).replace(/(^\s+|\s+$)/, '')} `, 'javascript');
+
+			// 	return {
+			// 		contents: [markdownString]
+			// 	};
+
+			// }
+		}
+	});
+
+	context.subscriptions.push(hover);
+
+	// // The command has been defined in the package.json file
+	// // Now provide the implementation of the command with registerCommand
+	// // The commandId parameter must match the command field in package.json
+	// let disposable = vscode.commands.registerCommand('cabinet-vsce.helloWorld', () => {
+	// 	// The code you place here will be executed every time your command is executed
+	// 	// Display a message box to the user
+	// 	vscode.window.showInformationMessage('Hello World from cabinet-vsce!');
+	// });
+
+	// context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() { }
